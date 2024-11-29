4eda25087475115815810723e3620f44965e2ffe
SergOrloff <6202818@gmail.com>
2024-11-29 23:16:49 +0300

    adding pub_date field into DB

--- zerocoder/news/forms.py
+++ zerocoder/news/forms.py
@@ -4,9 +4,10 @@ from .models import NewsPost
 class NewsPostForm(forms.ModelForm):
     class Meta:
         model = NewsPost
-        fields = ['title', 'short_description', 'text']
+        fields = ['title', 'short_description', 'text', 'pub_date']
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Заголовок новости'}),
-            'short_description': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Краткое описание новости'}),
+            'short_description': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Краткое описание новости'}),
             'text': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Новость'}),
+            'pub_date': forms.DateTimeInput(attrs={'class': 'form-control', 'placeholder': 'Дата публикации'}),
         }
--- zerocoder/news/models.py
+++ zerocoder/news/models.py
@@ -2,10 +2,12 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class NewsPost(models.Model):
+    objects = None
     title = models.CharField('Название новости', max_length=50)
     short_description = models.TextField('Краткое описание новости', max_length=200)
     text = models.TextField('Новость')
-    pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
+    # pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
+    pub_date = models.DateTimeField('Дата публикации')
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
